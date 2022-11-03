9a1c1aa6ea8e74c2a6e389ebed810142fa5bba2f
NPlanche <89564236+NPlanche@users.noreply.github.com>
2022-11-03 09:51:23 -0400

    Added all our work

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Qwl
+# QuantumOwl
---
+++ partials/navbar.php
@@ -0,0 +1,49 @@
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-----------------------Title----------------------->
+    <title>The Owl Survivor</title>
+
+    <!-----------------------CSS----------------------->
+    <link rel="stylesheet" href="../css/style.css">
+
+    <!---------------------Favicon--------------------->
+    <link rel="shortcut icon" href="../images/favicon.png">
+
+    <!---------Import Poppins from Google fonts--------->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <!---------Import Sigmar One from Google fonts--------->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Sigmar+One&display=swap" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Navbar toogle -->
+    <script src="https://kit.fontawesome.com/82f797382f.js" crossorigin="anonymous"></script>
+
+
+</head>
+
+<body>
+    <!--------------------Navigation-------------------->
+    <nav class="navigation">
+
+        <!--Menu-->
+        <ul class="menu" id = "navigation-bar">
+            <li><a href="../index.html" class="nav_links">Home</a></li>
+            <li><a href="game_page/game.php" class="nav_links">Game</a></li>
+            <li><a href="#" class="nav_links">Rules</a></li>
+            <li><a href="#" class="nav_links">Tips & Tricks</a></li>
+            <li><a href="#" class="nav_links">About</a></li>
+        </ul> 
+
+    </nav>
+
+
+
